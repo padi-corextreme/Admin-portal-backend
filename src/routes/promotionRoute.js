913e968acf7a0553d15c31256d19d36d938fb220
apoerakay <apoerak@gmail.com>
2024-04-24 11:43:35 +0000

    multer

--- src/routes/promotionRoute.js
+++ src/routes/promotionRoute.js
@@ -1,11 +1,20 @@
 import { Router } from 'express';
 import multer from 'multer';
 import { createPromotion, deletePromotion, getAllPromotion, updatePromotion } from '../controllers/promotion_controller.js';
+import { multerSaveFilesOrg } from 'multer-savefilesorg';
 
 const router = Router();
 
 // Set up multer storage for handling file uploads
-const upload = multer({dest: 'uploads/images'});
+// const upload = multer({dest: 'uploads/images'});
+
+const upload = multer({
+    storage: multerSaveFilesOrg({
+        serverPath:`https://savefiles.org/api/v1/uploads`,
+        apiAccessToken: process.env.SAVEFILESORG_API_KEY,
+        relativePath: '/admin/promotions/images/*'
+    })
+})
 
 // Route for creating promotions with file uploads
 router.post('/api/admin/promotions', upload.single('image'), createPromotion);
--- src/server.js
+++ src/server.js
@@ -2,50 +2,32 @@ import express from "express";
 import dotenv from "dotenv";
 import router from "./routes/accountRoute.js";
 import promoRouter from "./routes/promotionRoute.js";
-import proRoute from './routes/productRoute.js';
-import mongoose from "mongoose";
-import cors from 'cors';
+import proRoute from './routes/productRoute.js'
+import { mongoose } from "mongoose";
+import cors from 'cors'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-app.use(express.json());
-app.use(cors());
 
-// Serve static files from the 'uploads' directory
-app.use('/uploads', express.static('uploads'));
 
-const mongoUrl = process.env.MONGODB_URL;
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Database is running');
-  })
-  .catch((error) => {
-    console.error('Error connecting to database:', error);
-    process.exit(1); // Exit the process if unable to connect to the database
-  });
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(express.static('uploads'));
+
+
+const mongoUrl = process.env.MONGODB_URL
+mongoose.connect(mongoUrl).then(()=>{
+    console.log('Database is running')
+}).catch((error)=>console.log(error))
 
 const PORT = process.env.PORT || 8080;
 
-// Use each router separately
-app.use(router);
-app.use(promoRouter);
-app.use(proRoute);
 
-// Middleware to handle successful upload
-app.post('/uploads', (req, res) => {
-  // Handle successful upload
-  console.log('File uploaded successfully');
-  res.status(200).send('File uploaded successfully');
+app.use(router, promoRouter, proRoute)
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running! ${PORT}`);
 });
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running! ${PORT}`);
-});
-
