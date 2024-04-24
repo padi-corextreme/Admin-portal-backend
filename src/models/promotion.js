c05bc70f74d40efa5a0a56b984b16c3dd086f530
apoerakay <apoerak@gmail.com>
2024-04-24 18:05:50 +0000

    update of number

--- src/models/promotion.js
+++ src/models/promotion.js
@@ -6,7 +6,7 @@ const promotionSchema = new schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     image: { type: String },
-    discountType: { type: String, required: true },
+    discountType: { type: Number, required: true },
     discountAmount: { type: Number, required: true },
     categories: { type: String, enum: ['clothing', 'home appliances', 'fashion', 'furniture', 'groceries'] },
     createdBy: { type: mongoose.Types.ObjectId, required: true },
